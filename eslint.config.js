3cc3e6acfbdb7469a6760b83ddd7629c61b1eb19
hustle-dev <dlwoabsdk@gmail.com>
2025-10-09 23:00:39 +0900

    [env] Initialize project with essential configuration files
    * .gitignore, ESLint, Prettier, TypeScript, and Vite setup.
    * Add basic HTML structure and manifest for Chrome extension.

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
+import reactPlugin from 'eslint-plugin-react'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+])
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { crx } from '@crxjs/vite-plugin'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import zip from 'vite-plugin-zip-pack'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import manifest from './manifest.config.ts'
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), crx({ manifest }), zip({ outDir: 'release', outFileName: 'release.zip' })],
+  server: {
+    cors: {
+      origin: [/chrome-extension:\/\//],
+    },
+  },
+})
