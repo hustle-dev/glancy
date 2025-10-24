5a1c4bcd60346dab20632a926105b53bdc84583c
hustle-dev <dlwoabsdk@gmail.com>
2025-10-24 22:53:24 +0900

    [feat] Introduce Toast component with styling and animations
    * Added Toast component for displaying messages with a slide-up and slide-down animation.
    * Created associated SCSS styles for the Toast component, ensuring a responsive and visually appealing design.
    * Implemented a modular structure for the Toast component, enhancing reusability within the application.

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,22 @@
+import { Toast as RadixToast } from 'radix-ui'
+
+import css from './Toast.module.scss'
+
+interface Props {
+  open: boolean
+  message: string
+  onOpenChange: (open: boolean) => void
+}
+
+const Toast = ({ open, onOpenChange, message }: Props) => {
+  return (
+    <RadixToast.Provider swipeDirection="up" duration={3000}>
+      <RadixToast.Root className={css.root} open={open} onOpenChange={onOpenChange}>
+        <RadixToast.Description className={css.message}>{message}</RadixToast.Description>
+      </RadixToast.Root>
+      <RadixToast.Viewport className={css.viewport} />
+    </RadixToast.Provider>
+  )
+}
+
+export default Toast
