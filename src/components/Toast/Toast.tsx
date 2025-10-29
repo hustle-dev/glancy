6b5271a88fa259f584b46faac509c91e0ab0ce8c
hustle-dev <dlwoabsdk@gmail.com>
2025-10-30 00:21:40 +0900

    [feat] Enhance Toast component with viewport customization
    * Added optional viewPortClassName prop to Toast component for improved styling flexibility.
    * Utilized clsx for conditional class name management in the Toast viewport, enhancing modularity and reusability.

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { Toast as RadixToast } from 'radix-ui'
 
 import css from './Toast.module.scss'
@@ -5,16 +6,17 @@ import css from './Toast.module.scss'
 interface Props {
   open: boolean
   message: string
+  viewPortClassName?: string
   onOpenChange: (open: boolean) => void
 }
 
-const Toast = ({ open, onOpenChange, message }: Props) => {
+const Toast = ({ open, onOpenChange, viewPortClassName, message }: Props) => {
   return (
     <RadixToast.Provider swipeDirection="up" duration={2000}>
       <RadixToast.Root className={css.root} open={open} onOpenChange={onOpenChange}>
         <RadixToast.Description className={css.message}>{message}</RadixToast.Description>
       </RadixToast.Root>
-      <RadixToast.Viewport className={css.viewport} />
+      <RadixToast.Viewport className={clsx(css.viewport, viewPortClassName)} />
     </RadixToast.Provider>
   )
 }
