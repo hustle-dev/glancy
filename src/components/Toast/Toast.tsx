a745a550ba55e55bfeb87756decbbdaa45e2cf3a
hustle-dev <dlwoabsdk@gmail.com>
2025-10-30 18:19:03 +0900

    [feat] Enhance Toast component with additional customization options
    * Added optional rootClassName and descriptionClassName props to the Toast component for improved styling flexibility.
    * Utilized clsx for conditional class name management, enhancing modularity and reusability of the Toast component.

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -6,15 +6,17 @@ import css from './Toast.module.scss'
 interface Props {
   open: boolean
   message: string
+  rootClassName?: string
+  descriptionClassName?: string
   viewPortClassName?: string
   onOpenChange: (open: boolean) => void
 }
 
-const Toast = ({ open, onOpenChange, viewPortClassName, message }: Props) => {
+const Toast = ({ open, onOpenChange, rootClassName, descriptionClassName, viewPortClassName, message }: Props) => {
   return (
     <RadixToast.Provider swipeDirection="up" duration={2000}>
-      <RadixToast.Root className={css.root} open={open} onOpenChange={onOpenChange}>
-        <RadixToast.Description className={css.message}>{message}</RadixToast.Description>
+      <RadixToast.Root className={clsx(css.root, rootClassName)} open={open} onOpenChange={onOpenChange}>
+        <RadixToast.Description className={clsx(css.message, descriptionClassName)}>{message}</RadixToast.Description>
       </RadixToast.Root>
       <RadixToast.Viewport className={clsx(css.viewport, viewPortClassName)} />
     </RadixToast.Provider>
